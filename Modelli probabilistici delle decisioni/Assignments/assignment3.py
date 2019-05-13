2eddfc400f23fbd3733abd86101309564aa91300
belerico <belo.fede@outlook.com>
2019-05-13 10:20:06 +0200

    Fixed minor bug

--- Modelli probabilistici delle decisioni/Assignments/assignment3.py
+++ Modelli probabilistici delle decisioni/Assignments/assignment3.py
@@ -10,16 +10,24 @@ class assignment3():
 
     def __init__(
         self,
-        adj: numpy.ndarray, 
-        variables: list, 
+        variables: list,
+        adj: numpy.ndarray,  
         cpts: list
     ):
         """
-        adj: adj matrix as numpy array. adj[i, j] = 1 iff exists a directed edge from node j to node i
-        vars: list of variables names in topological order
-        cpts: list of cpts for every variable following the topological order of vars\n
-              Cpts are stored as array with only the true value, the topological order and combinatorial order, for example:
-              if the topoligical order is [H, W, A, J] and the cpt is
+        variables: list of variables names in topological order
+        adj: adj matrix as numpy array. adj[i, j] = 1 iff exists a directed edge from node j to node i. 
+             Nodes in rows and cols must follow the topological order specified by variables. So, if variables
+             contains ['A', 'B', 'C'], then adj will be
+             A B C 
+             ------
+             0 0 0 | A
+             1 0 0 | B
+             1 1 0 | C
+        cpts: list of cpts for every variable following the topological order of vars.
+              Cpts are arrays containing only the true values, which are stored based on the combinatorial order, 
+              for example, for the Bayesian net in this assignment:
+              if the topological order is [H, W, A, J] then the cpt for J is
               H, W, A | P(J|H,W,A)
               0  0  0 | 0.1
               0  0  1 | 0.6
@@ -43,28 +51,38 @@ class assignment3():
         assert(n > 0)
         assert(sum(pdf) == 1)
         assert(len(pdf) == len(values))
+        return self.__discrete_sampling(n, pdf, values)  
+    
+    def __discrete_sampling(self, n: int, pdf: Iterable, values: Iterable):
         # Pair up probabilities and values
         pdf_values = list(zip(pdf, values))
         # Sort them 
         pdf_values.sort(reverse=True)
         # Compute the cumulative sum over pdfs
-        cumulative = list(zip(list(accumulate([x[0] for x in pdf_values])), [x[1] for x in pdf_values]))
-        sample = [None] * n
+        # cdf_values = list(zip(list(accumulate([x[0] for x in pdf_values])), [x[1] for x in pdf_values]))
+        cdf_values = [pdf_values[0]] + [(pdf_values[i][0] + pdf_values[i-1][0], pdf_values[i][1]) for i in range(1, len(pdf_values))]
+        # sorted_indices = numpy.argsort(pdf)
+        # cdf = numpy.cumsum([pdf[i] for i in reversed(sorted_indices)])
+        # cdf_values = list(zip(cdf, [values[i] for i in reversed(sorted_indices)]))
+        sample = numpy.zeros(n)
         for i in range(0, n):
-            for p, value in cumulative:
+            for p, value in cdf_values:
                 rand = random.random()
                 if (rand <= p):
                     sample[i] = value
                     break
-        return sample   
+        return sample 
     
     def likelihood_weighting(self, n:int, target:str, evidences: dict):
         # Supposing we're working with boolean variables, then the possible outcome are two: true or false
+        # First value is the probability of target being false, the second is the probability of target being true
         W = numpy.zeros(2)
+        # Index of the target variables in the topoligical order
+        target_index = self.variables.index(target)
         # Algorithm from Norvig book
         for _ in range(0, n):
             x, w = self.weighted_sampling(evidences)
-            target_value = x[self.variables.index(target)]
+            target_value = x[target_index]
             W[target_value] = W[target_value] + w
         # Normalizing factor
         alpha = 1 / numpy.sum(W)
@@ -76,6 +94,7 @@ class assignment3():
         """
         # Algorithm from Norvig book
         w = 1
+        # Array storing the variables values sampled so far, always following the topological order
         x = numpy.empty(len(self.variables), dtype='u1')
         for i, var in enumerate(self.variables):
             parents = numpy.where(self.adj[i] == 1)[0]
@@ -101,7 +120,7 @@ class assignment3():
                     # Get index of the cpt row
                     row_cpt = int(''.join(str(i) for i in parents_values), 2)
                 # Update the evidences
-                x[i] = self.discrete_sampling(1, [self.cpts[i][row_cpt], 1 - self.cpts[i][row_cpt]], [1, 0])[0]
+                x[i] = self.__discrete_sampling(1, [self.cpts[i][row_cpt], 1 - self.cpts[i][row_cpt]], [1, 0])[0]
         return x, w
 
 if __name__ == "__main__":
@@ -121,5 +140,5 @@ if __name__ == "__main__":
         [0.1, 0.6, 0.3, 0.5, 0.95, 0.95, 0.95, 0.95] # P(J|H,W,A)
     ]
     evidences = {'A':1}
-    ass3 = assignment3(adj, variables, cpts)
+    ass3 = assignment3(variables, adj, cpts)
     print(ass3.likelihood_weighting(50000, 'J', evidences))
